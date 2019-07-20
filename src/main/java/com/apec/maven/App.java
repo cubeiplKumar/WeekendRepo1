b4ad9368f447d9324d665ed47bed899fffd5aa9f
cubeiplKumar <34553586+cubeiplKumar@users.noreply.github.com>
2019-07-20 11:13:45 +0530

    Add files via upload
    commit the new changes

---
+++ src/main/java/com/apec/maven/App.java
@@ -0,0 +1,13 @@
+package com.apec.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the world of Maven Projects!" );
+    }
+}
